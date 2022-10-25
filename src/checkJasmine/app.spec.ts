bd5263f37618c3f6dfb377a276ab238fbab91640
ahmed farag <react.codework@gmail.com>
2022-10-25 17:33:41 +0200

    formating

--- src/checkJasmine/app.spec.ts
+++ src/checkJasmine/app.spec.ts
@@ -1,15 +1,12 @@
 import request from 'supertest';
 import app from '../app';
 
-describe('Express TS Server', () => {
+describe('Server Statrs Normally', () => {
   it('expect app to be defined', () => {
     expect(app).toBeDefined();
   });
 
-  it('expect server to return 200 for route "/"', (done) => {
-    request(app)
-      .get('/')
-      .expect(200)
-      .end((err) => (err ? done.fail(err) : done()));
+  it('server returns status 200 on route "/"', (done) => {
+    request(app).get('/').expect(200).end((error) => (error ? done.fail(error) : done()));
   });
 });
