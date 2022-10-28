f5ae57c8e48f854388731a783f0d32f169474105
gitfarag <react.codework@gmail.com>
2022-10-29 00:17:23 +0200

    feat fix refactor

--- src/middlewares/logger.ts
+++ src/middlewares/logger.ts
@@ -1,29 +1,20 @@
 import { Request, Response, NextFunction } from "express";
 
-/**
- * Logger middleware
- *
- * @param req
- * @param res
- * @param next
- *
- * @returns {void}
- */
 const logger = (req: Request, res: Response, next: NextFunction): void => {
-  // record the time the request was made
+  // start timer
   const start = Date.now();
-  // log the request with start time
+  // display som info
   console.log(
-    `:: ${req.method} ${req.originalUrl} [Started ${new Date(
+    `:: :${req.method} ${req.originalUrl}:: Started ${new Date(
       start
     ).toLocaleString()}]`
   );
   res.on("finish", () => {
-    // count the duration of the request
-    const duration = Date.now() - start; // in milliseconds
+    // calculate time cost
+    const timeCost = Date.now() - start; // in milliseconds
     // log the request with duration
     console.log(
-      `:: ${req.method} ${req.originalUrl} ${res.statusCode} [Finished in ${duration}ms]`
+      `:: ${req.method} ${req.originalUrl} ${res.statusCode} ::cost ${timeCost}ms]`
     );
   });
   // call next middleware/function
